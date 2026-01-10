4e21ad3606d394ea50ded626253fb4a6d2e8daae
samuelgomes0309 <samuelgs0309@gmail.com>
2026-01-10 13:21:33 -0300

    FIX Hydration

--- src/providers/app.provider.tsx
+++ src/providers/app.provider.tsx
@@ -2,17 +2,10 @@
 
 import { ReactNode } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { CacheProvider } from "@emotion/react";
-import createCache from "@emotion/cache";
+import { CacheProvider } from "@chakra-ui/next-js";
 
 import { AuthProvider } from "@/contexts/AuthContext";
 import { system } from "./chakra.system";
-
-// Configuração do Emotion Cache para Chakra UI
-const emotionCache = createCache({
-	key: "css",
-	prepend: true,
-});
 
 interface AppProviderProps {
 	children: ReactNode;
@@ -20,7 +13,7 @@ interface AppProviderProps {
 
 export function AppProvider({ children }: AppProviderProps) {
 	return (
-		<CacheProvider value={emotionCache}>
+		<CacheProvider>
 			<ChakraProvider value={system}>
 				<AuthProvider>{children}</AuthProvider>
 			</ChakraProvider>
